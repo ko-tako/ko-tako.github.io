8bb5173890f9f886d1ddc5cd4d66c75a466747a3
ko-tako <koishizakigithubio+gravity@gmail.com>
2024-12-14 17:52:50 +0900

    Update service-worker.js

--- jihou/test/service-worker.js
+++ jihou/test/service-worker.js
@@ -1,1 +1,12 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetching:', event.request.url);
+});
